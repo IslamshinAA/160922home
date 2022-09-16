4fff9e80bdcc702b913a65227143ebd5e4503edd
A.Islamshin <arshavin0511@gmail.com>
2022-09-16 19:05:03 +0300

    task43

--- task/Program.cs
+++ task/Program.cs
@@ -2,27 +2,35 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-Console.Clear();
-Console.Write("Введите количество элементов в массиве: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int [] arr1 = new int [n];
-for(int i = 0; i < arr1.Length; i++) 
-{   Console.Write("Введите элемент массива: ");
-    arr1[i] = Convert.ToInt32(Console.ReadLine());   
-}
-Console.Write("Массив: [" + string.Join(", ", arr1) + "]");
-int res = 0;
-for (int i = 0; i < arr1.Length; i++)
-{  
-    if (arr1[i] > 0)
-       res = res + 1;      
-}
-Console.WriteLine();
-Console.Write("Количество положительных элементов в массиве :" + res);
+// Console.Clear();
+// Console.Write("Введите количество элементов в массиве: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int [] arr1 = new int [n];
+// for(int i = 0; i < arr1.Length; i++) 
+// {   Console.Write($"Введите элемент массива {i}: ");
+//     arr1[i] = Convert.ToInt32(Console.ReadLine());   
+// }
+// Console.Write("Массив: [" + string.Join(", ", arr1) + "]");
+// int res = 0;
+// for (int i = 0; i < arr1.Length; i++)
+// {  
+//     if (arr1[i] > 0)
+//        res = res + 1;      
+// }
+// Console.WriteLine();
+// Console.Write("Количество положительных элементов в массиве :" + res);
 
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-
-
-
+// Console.Clear();
+// Console.WriteLine("Введите координаты первой прямой: ");
+// double b1 = Convert.ToInt32(Console.ReadLine()), k1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты второй прямой: ");
+// double b2 = Convert.ToInt32(Console.ReadLine()), k2 = Convert.ToInt32(Console.ReadLine());
+// double x = (b2 - b1)/(k1 - k2);
+// double y = (k1 * x) + b1;
+// Console.WriteLine($"Координаты точки: {x} , {y}");
 
 
